a3ba4391f39c2d225a4822c6fa7ab58a36c159ea
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2025-09-29 14:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let student = {
+	name : "JS",
+	city : "Noida",
+	age : 50,
+}
+
+function getKeys(obj) {
+	return Object.keys(obj);
+}
+
+console.log(getKeys(student));
