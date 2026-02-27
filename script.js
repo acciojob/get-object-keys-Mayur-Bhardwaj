3ec1b1e83d9c4d697a7a1d0b8d020eb66119d130
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-02-27 16:24:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 // //your JS code here. If required.
-// var student = {
-// 	name : "JS",
-// 	city : "Noida",
-// 	age : 50,
-// }
 
-// function getKeys(obj) {
-// 	return Object.keys(obj);
-// }
-// getKeys(student)           // ["name"]
-// getKeys({name: "JS", age: 50})  // ["name", "age"]
-
-// Create the student object
-const student = {
-  name: "Mayur Bhardwaj",
-};
-
-// Function to get all keys from the object
 function getKeys(obj) {
   return Object.keys(obj);
 }
-
-// Testing the function
-console.log(getKeys(student)); // ["name"]
-// console.log(getKeys(student));
